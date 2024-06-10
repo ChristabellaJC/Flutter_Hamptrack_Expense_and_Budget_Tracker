2268a75c38bf4db22f0a8c4eba2d6647ed5a5ecd
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-10 17:24:46 +0700

    Create colors.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// PRIMARY
+var primaryColor = const Color(0xFFFAE588);
+var secondaryColor = const Color(0xFFFDF8E1);
+var toolbarIconColor = const Color(0xFFF9DC5C);
+var iconColor = const Color(0xFFE0AC00);
+
+// TEXT
+var primaryTextColor = const Color(0xFFE0AC00);
+var secTextColor = const Color(0xFFF9DC5C);
+var whiteColor = const Color(0xFFFFFFFF);
+
+var errorColor = Colors.red;
+
+// GRAPH
+var greenCol = const Color(0xFF06D6A0);
+var blueDarkCol = const Color(0xFF086788);
+var blueCol = const Color(0xFF25CBE5);
+var redCol = const Color(0xFFEF476F);
+var yellowCol = const Color(0xFFFFD166);
