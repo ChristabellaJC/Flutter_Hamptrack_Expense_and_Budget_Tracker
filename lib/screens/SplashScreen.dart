b751140a0b7f698d7dc0858ef5341a9278cf8ac2
535220166 <Christabella.535220166@stu.untar.ac.id>
2024-06-10 23:35:09 +0700

    SplashScreen initial creation
    Created the design for splash screen

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,83 @@
+import 'package:dev_hampter/utils/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:stroke_text/stroke_text.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [
+              0.2,
+              0.6,
+              0.9,
+            ],
+            colors: [
+              Color(0xFFFFED9E),
+              Color(0xFFFFE675),
+              Color(0xFFE9C317),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.asset('assets/images/splashMou.png', width: 180),
+              const SizedBox(height: 15),
+              StrokeText(
+                  text: "HampTrack",
+                  textStyle: TextStyle(
+                  fontFamily: 'BalooThambi2',
+                  fontSize: 47,
+                  color: whiteColor,
+                  fontWeight: FontWeight.w800,
+                  shadows: <Shadow>[
+                  Shadow(
+                    offset: const Offset(1.0, 5.0),
+                    blurRadius: 5.0,
+                    color: shadowColor,
+                  ),
+                ],
+                  ),
+                  strokeColor: textColor,
+                  strokeWidth: 5,
+
+                ),
+StrokeText(
+                  text: "HampTrack",
+                  textStyle: TextStyle(
+                  fontFamily: 'BalooThambi2',
+                  fontSize: 25,
+                  color: whiteColor,
+                  fontWeight: FontWeight.w800,
+                  shadows: <Shadow>[
+                  Shadow(
+                    offset: const Offset(1.0, 5.0),
+                    blurRadius: 5.0,
+                    color: shadowColor,
+                  ),
+                ],
+                  ),
+                  strokeColor: textColor,
+                  strokeWidth: 5,
+
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// PRIMARY
+var primaryColor = const Color(0xFFFAE588);
+var secondaryColor = const Color(0xFFFDF8e1);
+var toolbarIconColor = const Color(0xFFF9DC5C);
+var iconColor = const Color(0xFFE0AC00);
+
+// TEXT
+var textColor = const Color(0xFFE0AC00);
+var secTextColor = const Color(0xFFF9DC5C);
+var whiteColor = const Color(0xFFFFFFFF);
+var shadowColor = const Color.fromARGB(125, 70, 60, 7);
+
+var errorColor = Colors.red;
+
+// GRAPH
+var greenCol = const Color(0xFF06D6A0);
+var blueDarkCol = const Color(0xFF086788);
+var blueCol = const Color(0xFF25CBE5);
+var redCol = const Color(0xFFEF476F);
+var yellowCol = const Color(0xFFFFD166);
