4658ae9ef2c88ab43800624bac48e9c8402e5c27
Jessch22 <jessica.535220187@stu.untar.ac.id>
2024-06-23 17:14:35 +0700

    Update sizes.dart

--- lib/utils/sizes.dart
+++ lib/utils/sizes.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const EdgeInsetsGeometry padding = EdgeInsets.all(20);
+
+// FOR ACCOUNT PAGE (EDIT BUDGET, ABOUT, SETTINGS)
+const EdgeInsetsGeometry accPadding = EdgeInsets.all(10);
+const double titleAccFont = 30.0;
